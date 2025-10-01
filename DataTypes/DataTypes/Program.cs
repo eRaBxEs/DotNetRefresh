4209c6cd975e2d9d73e8453af05f1889a96d7dcb
erabx <erabx@EHIOJE_HENRY>
2025-10-01 23:28:46 +0100

    Add project files.

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 37;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+            long bigNumber = 9000000000L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+
+            double negative = -55.22D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float precision = 5.000001F;
+            Console.WriteLine(precision);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            decimal money = 99.9M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ String_Chars/String_Chars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Henry";
+            char letter = 'E';
+            Console.Write("My name is ");
+            Console.WriteLine(name);
+
+            Console.WriteLine();
+            Console.WriteLine(letter);
+
+            Console.ReadLine();
+        }
+    }
+}
