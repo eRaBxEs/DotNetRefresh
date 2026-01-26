a011bf2e4e0add857370c86d825d08af0d6b70d2
Ehioje Henry Erabor <ehiojee@yahoo.com>
2026-01-26 22:08:58 +0000

    simple show of modulus

---
+++ Modulus/Modulus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = 10;
+            int secondNum = 3;
+
+            Console.WriteLine(firstNum / secondNum); // 3.333 but will print 3 because the decimal is truncated
+            Console.WriteLine(firstNum % secondNum);
+
+            Console.WriteLine(1000 / 90);
+            Console.WriteLine(100 / 90);
+            Console.WriteLine(71 / 10);
+
+            Console.ReadLine();
+        }
+    }
+}
