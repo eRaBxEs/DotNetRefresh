9ef991e0d241bdb0da51731cb936367f6c2ad658
Ehioje Henry Erabor <ehiojee@yahoo.com>
2026-02-07 14:43:44 +0000

    simple intro to if and else if and else

--- ConsoleInputOutput/ConsoleInputOutput/Program.cs
+++ ConsoleInputOutput/ConsoleInputOutput/Program.cs
@@ -10,24 +10,37 @@ namespace ConsoleInputOutput
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello my name is Coder!");
 
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
-            //Console.WriteLine(name);
+            
 
             Console.Write("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(age);
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
 
-            //Console.Write("Your name is ");
-            //Console.Write(name);
-            //Console.Write(" and your age is ");
-            //Console.Write(age);
 
             Console.WriteLine("Your name is "+name+ " and your age is "+age);
 
-            Console.ReadLine();
+            // ==, >, >= , <, <= , !=, ||, &&
+                        
+            if (age < 0 || age > 150)
+            {
+                Console.WriteLine("Invalid age!");
+            }
+            else
+            {
+                if (age >= 18 && age <= 25)
+                {
+                    Console.WriteLine("You are between 18 and 25.");
+                }
+                else if (age >= 26)
+                {
+                    Console.WriteLine("You are 26 or older");
+                }
+            }
+
+                Console.ReadLine();
         }
     }
 }
