8cfef7760259f74b5332ebd46c42356960dfc7fb
Ehioje Henry Erabor <ehiojee@yahoo.com>
2026-02-07 13:25:30 +0000

    define variable for your use

---
+++ Constants/Constants/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int vat = 20;
+            const double percentVat = vat / 100D;
+           
+
+            int balance = 1000;
+
+            Console.WriteLine(balance * (vat / 100D));
+            Console.WriteLine(balance * percentVat);
+
+            const string version = "v.1.01";
+            Console.WriteLine(version);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StoringData/StoringData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoringData
+{
+    internal class Program
+    {
+        /**
+            1. Define a variable to hold your name
+            2. Define a variable to hold your phone number
+            3. Define a vatriable to hold your age
+            4. Print variables line by line to the screen
+            5. Extras: define variables using the var keyword
+         **/
+        static void Main(string[] args)
+        {
+            var name = "Henry";
+            var number = "0123456789";
+            var age = 23;
+
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
+
+            Console.ReadLine();
+        }
+    }
+}
