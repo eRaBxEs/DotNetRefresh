1e64004f0acde1daafc5002d0600da28030ceab6
Ehioje Henry Erabor <ehiojee@yahoo.com>
2026-02-07 16:26:50 +0000

    crack on with multiplication exercise

---
+++ ExerciseMultiplication/ExerciseMultiplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseMultiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the first number: ");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            string numberBInput = Console.ReadLine();
+            int numberB = Convert.ToInt32(numberBInput);
+
+            int answer = numberA * numberB;
+
+            Console.Write("Value of "+ numberA + " x "+ numberB+ " : ");
+            string answerInput = Console.ReadLine();
+            int finalInput = Convert.ToInt32(answerInput);
+
+            if (answer == finalInput)
+            {
+                Console.WriteLine("Well done!");
+            }
+            else
+            {
+                Console.WriteLine("Close but it was wrong!");
+            }
+
+
+           Console.ReadLine();
+        }
+    }
+}
