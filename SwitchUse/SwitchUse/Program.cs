c29b51539bd3cae6e97ac76f81725335d3b02921
Ehioje Henry Erabor <ehiojee@yahoo.com>
2026-02-07 21:45:48 +0000

    using switch statement

---
+++ SwitchUse/SwitchUse/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchUse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            
+            //if (day == 1)
+            //{
+            //    Console.WriteLine("Mon");
+            //}
+            //else if(day == 2)
+            //{
+            //    Console.WriteLine("Tue");
+            //}
+
+            switch(day)
+            {
+                case 1: Console.WriteLine("Mon");
+                    break;
+                case 2:Console.WriteLine("Tue");
+                    break;
+                case 3: Console.WriteLine("Wed");
+                    break;
+                case 4: Console.WriteLine("Thur");
+                    break;
+                case 5: Console.WriteLine("Fri");
+                    break;
+                case 6: Console.WriteLine("Sat");
+                    break;
+                case 7: Console.WriteLine("Sun");
+                    break;
+                default: Console.WriteLine("Invalid, Enter a value from 1 to 7");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
