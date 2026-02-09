0cc9dfcb9cb4a577b3dfb1bc1df3d2a44f8994e6
Ehioje Henry Erabor <ehiojee@yahoo.com>
2026-02-09 14:55:47 +0000

    a quick use of loops

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hi");
+            //Console.WriteLine("Hi");
+            //Console.WriteLine("Hi");
+            //Console.WriteLine("Hi");
+            //Console.WriteLine("Hi");
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("Hi");
+            }
+
+            Console.WriteLine("======== Even numbers below =======");
+
+            for (int i = 0; i <= 10; i += 2) 
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("======== Odd numbers below =======");
+
+            for (int i = 1; i <= 10; i += 2) 
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
